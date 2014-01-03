51d7a53352e147af0a5b9b8979de0e6e86cf19bd
lianglili <lianglili123@gmail.com>
2014-01-03 17:05:10 +0900

    innerclass test.

---
+++ multi-thread-sample/src/main/java/my/sample/mywebserver/TaskExecutionWebServer.java
@@ -0,0 +1,29 @@
+package my.sample.mywebserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class TaskExecutionWebServer {
+    private static final int NTHREADS=100;
+    private static final Executor exec=Executors.newFixedThreadPool(NTHREADS);
+    public static void main(String[] args) throws IOException
+    {
+	ServerSocket socket=new ServerSocket(80);
+	while(true){
+	    final Socket connection=socket.accept();
+	    Runnable task=new Runnable(){
+
+		public void run() {
+		    //適当な処理...
+		    System.out.println(connection.toString());
+		}
+		
+	    };
+	    exec.execute(task);
+	    socket.close();
+	}
+    }
+}
