59e115cb854ae7a8f1d7f082ed5518835f8339e4
lianglili <lianglili123@gmail.com>
2014-12-23 00:20:11 +0900

    aop logging runs.(delete comments)

--- helloworld/src/main/java/my/sample/helloworld/aop/LoggingAspect.java
+++ helloworld/src/main/java/my/sample/helloworld/aop/LoggingAspect.java
@@ -2,10 +2,8 @@ package my.sample.helloworld.aop;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -22,9 +20,5 @@ public class LoggingAspect {
 	private void afterMapping(){
 		logger.info("after mapping.");
 	}
-	
-//	@Around("execution(* *Mapping(..))")
-//	private void aroundMapping(){
-//		logger.info("after mapping.");
-//	}
+
 }
